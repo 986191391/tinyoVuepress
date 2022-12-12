5da12b14c68dd0ef4fc268b373e0bd3198135cf2
yangzefeng <986191391@qq.com>
2022-12-12 23:19:35 +0800

    抽奖逻辑编写

--- src/views/pc/lottery/lottery.vue
+++ src/views/pc/lottery/lottery.vue
@@ -3,7 +3,8 @@
     <CoolNav />
     <main>
       <div class="lottery-main">
-        <img class="zhizhen" src="@/assets/lottery/zhizhen.jpg" />
+        <span class="go-font" @click="goLottery">{{ timer ? '确定确定' : '开始抽奖'}}</span>
+        <img class="zhizhen" src="@/assets/lottery/zhizhen.jpg" :style="{ transform: `rotate(${rotateDeg}deg)` }"/>
       </div>
     </main>
   </div>
@@ -16,11 +17,69 @@ export default {
   components: { CoolNav },
   data () {
     return {
+      timer: null,
+      rotateDeg: 0,
+      lock: false
     }
   },
   computed: {
   },
   methods: {
+    goLottery () {
+      if (this.lock) return
+      let rotateDeg = 0
+      if (!this.timer) {
+        this.rotateDeg = 0
+        this.timer = setInterval(() => {
+          if (rotateDeg < 90) {
+            this.rotateDeg = rotateDeg += 3
+          } else if (rotateDeg > 90 && rotateDeg < 200) {
+            this.rotateDeg = rotateDeg += 5
+          } else if (rotateDeg > 180 && rotateDeg < 300) {
+            this.rotateDeg = rotateDeg += 8
+          } else {
+            this.rotateDeg = rotateDeg += 10
+          }
+          console.log('rotateDeg', this.rotateDeg)
+        }, 10)
+      } else {
+        this.lock = true
+        setTimeout(() => {
+          let stopDeg = Math.ceil(Math.random() * 360)
+          let targetDeg = Math.ceil(this.rotateDeg / 360) * 360 + stopDeg
+          this.clearTimer()
+          this.timer = setInterval(() => {
+            console.log('stopDeg', stopDeg)
+            console.log('targetDeg', targetDeg)
+            console.log('this.rotateDeg=', this.rotateDeg)
+            const curVal = targetDeg - this.rotateDeg
+            console.log('curVal', curVal)
+            if ( curVal === 0) {
+              this.clearTimer()
+            } else if (curVal > 720) {
+              this.rotateDeg = this.rotateDeg + 10
+            } else if (curVal > 360 && curVal <= 720 ) {
+              this.rotateDeg = this.rotateDeg + 8
+            } else if (curVal > 200 && curVal <= 360 ) {
+              this.rotateDeg = this.rotateDeg + 8
+            } else if (curVal > 50 && curVal <= 200 ) {
+              this.rotateDeg = this.rotateDeg + 6
+            } else if (curVal > 5 && curVal <= 50) {
+              this.rotateDeg = this.rotateDeg + 2
+            } else if (curVal > 0 && curVal <= 5) {
+              this.rotateDeg = this.rotateDeg + 1
+            }
+          }, 10)
+        }, 2000)
+      }
+    },
+    clearTimer () {
+      if (this.timer) {
+        clearInterval(this.timer)
+        this.lock = false
+        this.timer = null
+      }
+    }
   },
   mounted () {
   } 
@@ -48,14 +107,33 @@ export default {
         background-repeat: no-repeat;
         position: relative;
 
+        .go-font {
+          position: absolute;
+          left: calc(50% - 34.3px);
+          top: 245px;
+          z-index: 2;
+          user-select: none;
+          cursor: pointer;
+        }
+
         .zhizhen {
           width: 108.6px;
           height: 152.2px;
           position: absolute;
           top: 160px;
           left: calc(50% - 54.3px);
-          transform: rotate(20deg);
+          transform: rotate(0deg);
+          // animation: zhizhenrotate 0.5s linear infinite;
           transform-origin: center 98px;
+        }
+      }
+
+      @keyframes zhizhenrotate {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
         }
       }
     }
